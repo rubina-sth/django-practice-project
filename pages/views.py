e3718a10fbc201e89da6afe411a8765a8c5c7458
rubina-sth <sth.rubina@gmail.com>
2021-05-16 14:13:28 +0700

    initial commit

---
+++ pages/views.py
@@ -0,0 +1,11 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+from django.http import HttpResponse
+
+
+def index(request):
+    return render(request, 'pages/index.html')
+
+
+def about(request):
+    return render(request, 'pages/about.html')
